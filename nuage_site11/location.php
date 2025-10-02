ff7502d9d6face74764d44c1315fa36261b10b21
Terrance <137191378+TerragravLLC@users.noreply.github.com>
2025-10-01 23:55:46 -0400

    update

--- nuage_site11/location.php
+++ nuage_site11/location.php
@@ -674,5 +674,20 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 </script>
 
+<div id="appModal" class="modal-overlay" style="display:none;">
+  <div class="modal-box">
+    <h2>Download the Glofox App</h2>
+    <p>
+    Please download the Glofox app, search <strong>NuAge Fitness Studio</strong> and register.<br>
+      Once logged in, you’ll be able to:<br>
+      • Access your account<br>
+      • Purchase membership<br>
+      • Book classes<br>
+      • And more.
+    </p>
+    <div class="modal-actions">
+      <button onclick="window.open('https://apps.apple.com/app/id916224471','_blank')">Apple</button>
+      <button onclick="window.open('https://play.google.com/store/apps/details?id=com.glofox&hl=en','_blank')">Google</button>
+    </div>
 </body>
 </html>
