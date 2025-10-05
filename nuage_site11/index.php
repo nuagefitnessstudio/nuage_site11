ef4d708588177b1d77f078346641edd24855e379
Terrance <137191378+TerragravLLC@users.noreply.github.com>
2025-10-05 19:13:59 -0400

    update

--- nuage_site11/index.php
+++ nuage_site11/index.php
@@ -594,7 +594,7 @@ a, button { -webkit-tap-highlight-color: transparent; }
     </p>
     <div class="modal-actions">
       <button onclick="window.open('https://apps.apple.com/app/id916224471','_blank')">Apple</button>
-      <button onclick="window.open('https://play.google.com/store/apps/details?id=com.glofox&hl=en','_blank')">Google</button>
+      <button onclick="window.open('https://play.google.com/store/apps/details?id=ie.zappy.fennec.oneapp_glofox&hl=en_US','_blank')">Google</button>
     </div>
     <button class="modal-close" onclick="document.getElementById('loginModal').style.display='none'">×</button>
   </div>
